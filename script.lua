31693f71ff27e13dcea0281b69dc42348f34d65a
Nigel Atkinson <merlinblack@users.noreply.github.com>
2017-06-23 12:27:34 +1000

    Progress with Lua binding.
    However it looks like a POD (Plain old data) binding might be a good
    idea.

--- script.lua
+++ script.lua
@@ -1,5 +1,20 @@
 print 'Lua Script'
 
+prev = 0
+
 function update( t )
-    print( t )
+    if t - prev > 1 then
+        prev = t
+        hello.text, world.text = world.text, hello.text
+    end
 end
+
+hello = Text( "Hello", fixed_font, 100, 100 )
+world = Text( "World", fixed_font, 120, 120 )
+print( hello )
+print( fixed_font )
+
+renderlist:add( hello )
+renderlist:add( world )
+
+print( 'Completed' );
