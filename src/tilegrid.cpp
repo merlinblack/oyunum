f7fb22b7d3c0c622b5af6e7360cac2fafe9698e2
Nigel Atkinson <merlinblack@users.noreply.github.com>
2017-07-06 16:20:03 +1000

    Add TileGrid and test data.

---
+++ src/tilegrid.cpp
@@ -0,0 +1,39 @@
+#include "tilegrid.h"
+
+void TileGrid::render()
+{
+    int x = dx;
+    int y = dy;
+    int column = 0;
+
+    al_hold_bitmap_drawing( true );
+
+    for( auto const tile : data ) {
+        if( tile != 0 ) {
+            if( column >= width ) {
+                column = 0;
+                y += 32;
+                x = dx;
+            }
+            al_draw_bitmap(
+                    tileSet->getTile( tile ),
+                    x, y,
+                    0
+                    );
+            column++;
+            x += 32;
+        }
+    }
+
+    al_hold_bitmap_drawing( false );
+}
+
+inline int TileGrid::getTile( int x, int y )
+{
+    return data[getOffset(x,y)];
+}
+
+inline void TileGrid::setTile( int x, int y, int tile )
+{
+    data[getOffset(x,y)] = tile;
+}
---
+++ src/tilegrid.h
@@ -0,0 +1,54 @@
+#ifndef TILEGRID_H
+#define TILEGRID_H
+
+#include "renderlist.h"
+#include "tileset.h"
+#include <vector>
+#include <memory>
+
+using TileGridData = std::vector<int>;
+
+class TileGrid : public Renderable
+{
+    TileSetPtr tileSet;
+    int width;
+    int height;
+    int dx;
+    int dy;
+
+    TileGridData data;
+
+    inline int getOffset( int x, int y )
+    {
+        return (y * width) + x;
+    }
+
+    public:
+
+    TileGrid( TileSetPtr& ts, int w, int h ) 
+        : tileSet(ts)
+        , width(w)
+        , height(h)
+        , dx(0)
+        , dy(0)
+    {}
+
+    void setData( TileGridData newData )
+    {
+        data.swap( newData );
+    }
+
+    void setXY( int x, int y )
+    {
+        dx = x, dy = y;
+    }
+
+    void render();
+
+    inline int getTile( int x, int y );
+    inline void setTile( int x, int y, int tile );
+};
+
+using TileGridPtr = std::shared_ptr<TileGrid>;
+
+#endif // TILEGRID_H
