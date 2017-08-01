1e5a791e0efdcbc1174a789569a1fb4b137b70f5
Nigel Atkinson <nigel@atkinson.kiwi>
2017-08-01 13:05:16 +1000

    Binding boiler plate

---
+++ src/lb_template.h
@@ -0,0 +1,54 @@
+// Binding boiler plate code for copying.
+#ifndef LB_MYCLASS_H
+#define LB_MYCLASS_H
+
+#include "LuaBinding.h"
+
+using namespace ManualBind;
+
+struct MyClassBinding : public Binding<MyClassBinding,MyClass>
+{
+    static constexpr const char* class_name = "MyClass";
+
+    static luaL_Reg* members()
+    {
+        static luaL_Reg members[] = {
+            { "__upcast", upcast },
+            { nullptr, nullptr }
+        };
+        return members;
+        //return nullptr;
+    }
+
+    static bind_properties* properties()
+    {
+        static bind_properties properties[] = {
+            { nullptr, nullptr, nullptr }
+        };
+        return properties;
+        //return nullptr;
+    }
+
+    static int create( lua_State* L )
+    {
+        MyClassPtr b = std::make_shared<MyClass>();
+
+        push( L, b );
+
+        return 1;
+    }
+
+    static int upcast( lua_State* L )
+    {
+        MyClassPtr b = fromStack( L, 1 );
+
+        BasePtr r = std::dynamic_pointer_cast<Base>( b );
+
+        BaseBinding::push( L, r );
+
+        return 1;
+    }
+
+};
+
+#endif //LB_MYCLASS_H
