a36ea0f11e20c6a35f851a15e74a046107cb3752
Nigel Atkinson <merlinblack@users.noreply.github.com>
2017-08-30 11:37:10 +1000

    Sprite ordering in atlases

--- scripts/gensprites.lua
+++ scripts/gensprites.lua
@@ -6,6 +6,6 @@ packatlas( 'data/sprites/adventuregirl', 'data/sprites', 'adventuregirl', 768, 1
 packatlas( 'data/sprites/ninjagirlnew', 'data/sprites', 'ninjagirlnew', 1024, 1024, .18, 0 ):process()
 packatlas( 'data/sprites/ninjaadventurenew', 'data/sprites', 'ninjaadventurenew', 1024, 1024, .18, 0 ):process()
 packatlas( 'data/sprites/jackfree', 'data/sprites', 'jackfree', 1024, 1024, .18, 0 ):process()
-packatlas( 'data/sprites/cards', 'data/sprites', 'cards', 1024, 1024, 1, 0 ):process()
+packatlas( 'data/sprites/cards', 'data/sprites', 'cards', 1024, 1024, 1, 5 ):process()
 
 print( 'Finished generating sprite atlases.' )
