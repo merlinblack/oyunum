9fe05758cd2f8922ef5933ad44467aea28e05952
Nigel Atkinson <merlinblack@users.noreply.github.com>
2017-09-04 14:06:42 +1000

    Make getdata.sh work for eveyone
    Not just me :-)

--- getdata.sh
+++ getdata.sh
@@ -1,2 +1,3 @@
-rm -rf data/*
-tar xvf ~/Dropbox/GameDev/oyunum/data.tar.gz
+mv data{,.old} && echo 'Moved old data to data.old'
+wget https://www.dropbox.com/s/ystc9qq6dqxy2nu/data.tar.gz?dl=0 -O data.tar.gz
+tar xf data.tar.gz && echo 'Extracted tar archive to data directory.' && rm -v data.tar.gz && echo 'Removed tar archive' && rm -rf data.old && echo 'Deleted data.old'
