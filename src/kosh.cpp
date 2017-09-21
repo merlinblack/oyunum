d424a63eaf5db05a76fc587582191b2b01c85392
Nigel Atkinson <merlinblack@users.noreply.github.com>
2017-09-21 15:26:29 +1000

    Beginning of a command line script tool
    kosh (koş) "run"

---
+++ src/kosh.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+using std::cout;
+using std::endl;
+
+int main( int argc, char* argv[] )
+{
+    cout << "Koş ay betiği" << endl;
+
+    return EXIT_SUCCESS;
+}
