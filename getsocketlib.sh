7da405e32070e8e59dd58b26c3e41142eddc8df0
Nigel Atkinson <merlinblack@users.noreply.github.com>
2017-10-18 13:23:32 +1100

    Bash script to grab and locally install luasocket

---
+++ getsocketlib.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# Grab, build and localally install luasocket
+# for remote debugging.
+
+DESTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/build/lualib
+
+git clone https://github.com/diegonehab/luasocket.git
+
+cd luasocket
+
+mkdir -p results
+
+make LUAV=5.2 prefix=$(realpath results) install
+
+mkdir -vp $DESTDIR
+
+cp -vr results/lib/lua/5.2/* $DESTDIR
+cp -vr results/share/lua/5.2/* $DESTDIR
